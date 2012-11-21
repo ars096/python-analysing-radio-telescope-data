b512af62197af736af38f2aac32fd3c513829ee1
Atsushi NISHIMURA <ars096@gmail.com>
2012-11-21 09:19:58 +0900

    CREATE: spectrum.py
    change __init__.py file

--- analyse/__init__.py
+++ analyse/__init__.py
@@ -11,3 +11,4 @@ from basefunc.loadfits import loadfits
 from basefunc.savefits import savefits
 from basefunc.makespec import makespec
 from basefunc.baseline_fit import basefit
+from basefunc.spectrum import get_tpeak as tpeak
